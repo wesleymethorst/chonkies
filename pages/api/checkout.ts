a4ddcb4838c1d7b673d4e061166627deddc40806
Wesley <122568999+wesleymethorst@users.noreply.github.com>
2025-04-24 22:54:58 +0200

    Update Stripe API version to 2025-03-31.basil

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2025-03-31.basil",
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
